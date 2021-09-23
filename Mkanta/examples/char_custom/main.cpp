8c86b0163008a29dc3f303529e31e8e0a082e30f
tyanmahou <tyanmahou@gmail.com>
2021-09-23 19:14:25 +0900

    add chartype change sample

---
+++ Mkanta/examples/char_custom/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+// You can change base char type
+#define MKANTA_CHARTYPE wchar_t
+#include <mkanta.hpp>
+
+struct Test
+{
+    [[REFLECTION(func)]]
+    static void func(int a) { std::cout << a << std::endl; }
+};
+
+int main()
+{
+    using mkanta::reflect;
+
+    // with wchar_t
+    if (auto func = reflect<Test>::find<void(*)(int)>(L"func")) {
+        (*func)(10);
+    }
+}

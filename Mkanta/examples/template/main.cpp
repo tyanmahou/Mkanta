4a9e091205f780400295a600c6bc152110798f88
tyanmahou <tyanmahou@gmail.com>
2021-12-22 23:03:26 +0900

    add template example

---
+++ Mkanta/examples/template/main.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <mkanta.hpp>
+
+template<class T>
+struct Test
+{
+    [[REFLECTION(func)]]
+    void func(T a) { std::cout << a << std::endl; }
+};
+
+// Need explicit 
+template struct Test<int>;
+template struct Test<double>;
+
+int main()
+{
+    using mkanta::reflect;
+
+    if (auto func = reflect<Test<int>>::find<void(Test<int>::*)(int)>("func")) {
+        Test<int> a;
+        (a.*func)(10);
+    }
+    if (auto func = reflect<Test<double>>::find<void(Test<double>::*)(double)>("func")) {
+        Test<double> a;
+        (a.*func)(10.5);
+    }
+}
